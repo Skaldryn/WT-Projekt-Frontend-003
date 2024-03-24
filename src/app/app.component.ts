6d72f6ab6f7c3548f03432d40b16373cc1e4e795
Alice Boeddinghaus <s0587021@htw-berlin.de>
2024-03-24 12:55:51 +0100

    First Commit?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./footer/footer.component";
+import {NavComponent} from "./nav/nav.component";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
-  selector: 'app-root',
+  selector: 'ACB-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import {HomeComponent} from "./home/home.component";
+import {ListComponent} from './list/list.component';
+import {ProfileComponent} from './profile/profile.component';
+import {MakeprofileComponent} from "./makeprofile/makeprofile.component";
+
+export const routes: Routes = [
+
+  { path: "home", component: HomeComponent},
+  { path: "list", component: ListComponent},
+  { path: "profile", component: ProfileComponent},
+  { path: "makeprofile", component: MakeprofileComponent},
+
+];
