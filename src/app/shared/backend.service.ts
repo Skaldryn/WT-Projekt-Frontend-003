1f49d6ab239cfa99aa50f35c9dd701a7de1785c9
Alice Boeddinghaus <s0587021@htw-berlin.de>
2024-03-25 13:59:09 +0100

    Crud Funktionen update

--- src/app/shared/backend.service.ts
+++ src/app/shared/backend.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Plant } from './plant';
 import { Observable } from 'rxjs';
+import {end} from "@popperjs/core";
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,45 @@ import { Observable } from 'rxjs';
 
 export class BackendService {
 
-  constructor() { }
+  backendUrl = 'http://localhost:4000';
+
+  constructor(private http: HttpClient) { }
+
+  // get all Plants
+  getAllPlants(): Observable<Plant[]>
+  {
+    let endpoint = '/plants';
+    return this.http.get<Plant[]>(this.backendUrl + endpoint);
+  }
+
+  // delete one Plant
+  deleteOnePlant(id:string): Observable<any>
+  {
+    let endpoint = '/plants';
+    return this.http.delete<any>(this.backendUrl + end + "/" +  id);
+  }
+
+  // get / fetch one plant
+  getOnePlant(id:string):Observable<Plant>
+  {
+    let endpoint = '/plants';
+    return this.http.get<Plant>(this.backendUrl + endpoint + '/' + id);
+  }
+
+  // create / post / plant :) a new Plant
+  createNewPlant(plant:Plant): Observable<Plant>
+  {
+    let endpoint = '/plants';
+    return this.http.post<Plant>(this.backendUrl + endpoint, plant);
+  }
+
+  // update a plant
+  updateOnePlant(plant :Plant, id : string): Observable<Plant>
+  {
+    let endpoint = '/plants';
+    return this.http.put<Plant>(this.backendUrl + endpoint + '/' + id, plant);
+
+  }
+
+
 }
--- src/app/shared/plant.ts
+++ src/app/shared/plant.ts
@@ -1,2 +1,13 @@
 export interface Plant {
+
+  id:string;
+  dtName:string;
+  latName:string;
+  vGebiet:string;
+  beschr:string;
+  verwend:string;
+  ofgattung:string;
+  anbauZeit:string;
+
+
 }
