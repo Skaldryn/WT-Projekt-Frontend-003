37bdb58230f9bc19128125f2d1891dc1b4485646
Alice Boeddinghaus <s0587021@htw-berlin.de>
2024-03-25 10:03:35 +0100

    shared directory

---
+++ src/app/shared/backend.service.ts
@@ -0,0 +1,15 @@
+
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Plant } from './plant';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BackendService {
+
+  constructor() { }
+}
