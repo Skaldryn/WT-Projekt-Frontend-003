716258f35b1c23f8845b92c3f233e045ae2fc23f
Alice Boeddinghaus <s0587021@htw-berlin.de>
2024-03-25 15:26:03 +0100

    HTTPClientModule added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
-import { Routes } from '@angular/router';
 
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import {HomeComponent} from "./home/home.component";
 import {ListComponent} from './list/list.component';
@@ -9,8 +10,15 @@ import {MakeprofileComponent} from "./makeprofile/makeprofile.component";
 export const routes: Routes = [
 
   { path: "home", component: HomeComponent},
-  { path: "list", component: ListComponent},
-  { path: "profile", component: ProfileComponent},
+  //{ path: "list", component: ListComponent},
+  { path: "plant", component: ListComponent},
+  //{ path: "profile", component: ProfileComponent},
+  { path: "plant/:id", component: ProfileComponent},
   { path: "makeprofile", component: MakeprofileComponent},
 
 ];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+
+
+
+import { Component, OnInit, inject } from '@angular/core';
+import { BackendService } from '../shared/backend.service';
+import { Plant } from '../shared/plant';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  //imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+    ngOnInit(): void{
+      //this.readAll();
+    }
+
+  //constructor(private bs: BackendService) {}
+
+  //bs = inject(BackendService)
+  plants: Plant[] = [];
+
+/*
+  readAll(): void {
+    this.bs.getAllPlants().subscribe(
+        {
+          next: (response) => {
+            this.plants = response;
+            console.log(this.plants);
+            return this.plants;
+          },
+          error: (err) => console.log(err),
+          complete: () => console.log('getAllPlants() completed')
+        })
+  }
+
+
+
+
+  delete(id: string): void {
+    console.log('id', id)
+    this.bs.deleteOnePlant(id).subscribe(
+        {
+          next: (response) => {
+            console.log(response);
+            this.readAll();
+          },
+          error: (err) => console.log(err),
+          complete: () => console.log('deleting completed')
+        })
+
+
+  }
+
+
+
+
+
+
+
+ */
+
+
+
+
+
 
 }
